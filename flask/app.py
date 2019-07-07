7b773d2a644fcb841f17b5b8c695baf28a7e9d91
Ko Tae Young <koty08@naver.com>
2019-07-07 21:01:39 +0900

    20190707 flask 파일 추가

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,5 +6,44 @@ app = Flask(__name__)
 def index():
     return 'Hello world'
 
+@app.route('/cakes/')
+def cakes():
+    return 'Yummy cakes!'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'User %s' % username
+
+@app.route('/user/<username>/<int:age>')
+def show_user_profile_age(username, age):
+    #return 'Username %s, 나이 %d' % (username,age)
+    return render_template('index.html', username=username, age=age)
+
+@app.route('/forminput/')
+def forminput():
+    return render_template('forminput.html')
+
+@app.route('/method/', methods=['GET', 'POST'])
+def method():
+    if request.method == 'POST':
+        return "Post"
+    else:
+        return "Get"
+
+@app.route('/login/')
+def login():
+    username = request.args.get('name')
+    return render_template('index.html', username=username)
+
+@app.route('/forminput1/')
+def forminput1():
+    return render_template('forminput1.html')
+
+@app.route('/login/', methods=['POST'])
+def login_post():
+    username = request.form['username']
+    password = request.form['password']
+    return render_template('index.html', username=username, password = password)
+    
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ flask/app1.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('form_input.html')
+
+@app.route('/login/', methods=['POST'])
+def login_post():
+    result = request.form
+    return render_template('form_result.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
